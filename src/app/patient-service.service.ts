1bfb63e15c7ab2629d317933b1211033ee3c79f0
Yasmine Moussaoui <yasminemoussaoui@MacBook-Air-de-Yasmine.local>
2024-11-09 17:03:32 +0100

    adding the service

---
+++ src/app/patient-service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientServiceService {
+
+  private apiUrl = 'http://localhost:8080/api';
+
+  constructor(private http: HttpClient) {}
+
+
+  getData(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/data-endpoint`);
+  }
+}
